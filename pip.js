fb7ebe97f02764ac359f511393de0a82c5c19966
Greg Whitworth <gwhit@microsoft.com>
2020-05-02 22:58:02 -0700

    Added readme

--- pip.js
+++ pip.js
@@ -2,31 +2,59 @@ function addVideoEls() {
     let videoEls = document.querySelectorAll('video');
 
     let pipButton = document.createElement('button');
-    pipButton.setAttribute('class', 'pipvid');
-    let img = document.createElement('img');
-    img.src = chrome.runtime.getURL('icon.png');
-    pipButton.append(img);
+    pipButton.setAttribute('class', 'pipvid pipvid-pip');
+    pipButton.setAttribute('title', 'Make video picture in picture');
+    pipButton.setAttribute('data-pipvid-action', 'pip');
+    let pipImage = document.createElement('img');
+    pipImage.src = chrome.runtime.getURL('icon.png');
+    pipButton.append(pipImage);
+
+    let pipVidControls = document.createElement('div');
+    pipVidControls.setAttribute('class', 'pipvid-controls');
+    pipVidControls.append(pipButton);
 
     let vcount = 0;
     videoEls.forEach((video) => {
         vcount++;
         video.setAttribute('data-pipvid-id', `pv-${vcount}`);
         pipButton.setAttribute('data-pipvid', `pv-${vcount}`);
-        video.parentNode.prepend(pipButton);
+        video.parentNode.prepend(pipVidControls);
     });
-    triggerPip();
+    triggerActions();
 }
 
-function triggerPip() {
+function triggerActions() {
     let pipButtons = document.querySelectorAll('.pipvid');
 pipButtons.forEach((el) => {
         el.addEventListener('click', () => {
             let pipVidId = el.getAttribute('data-pipvid');
+            let pipVidAction = el.getAttribute('data-pipvid-action');
             let selector = `[data-pipvid-id=${pipVidId}]`;
             let vid = document.querySelector(selector);
-            vid.requestPictureInPicture();
+
+            if (pipVidAction == "pip") {
+                triggerPip(vid);
+                return;
+            }
+
+            return;
         });
     });
 }
 
+function cleanSlate() {
+    let pipvidControls = document.querySelectorAll('.pipvid-controls');
+    pipvidControls.forEach((el) => el.parentNode.removeChild(el));
+}
+
+function triggerPip(vid) {
+    if (vid) {
+        if (document.pictureInPictureElement) {
+            document.exitPictureInPicture();
+        }
+        vid.requestPictureInPicture();
+    }
+}
+
+cleanSlate();
 addVideoEls();
---
+++ readme.md
@@ -0,0 +1,32 @@
+![PIPvid](https://github.com/gregwhitworth/pipvid/300.png "PIPvid")
+
+## Overview
+
+PIPvid effectively adds a little icon to the top HTML video elements after you click the extension icon. This icon allows you to put the videos into PIP mode.
+
+This leverages the standardized PIP API. I created this since so many websites haven't updated their sites, for one reason or another, to leverage the API. Ultimately this forces users to hack around it by making small browser windows and leveraging OS controls to keep the browser window on top.
+
+## Sites where it works
+
+While this works with a lot of websites that have a fairly common HTML 5 video setup. Sites that I have successfully tested PIPvid on are:
+
+* ESPN
+* Youtube
+* Mixer.com
+* Twitch.com
+* CNN
+* Netflix
+* Disney Plus
+
+### ...and where it doesn't
+
+Sites that manipulate the DOM a lot or adjust the styles upon playing in such a manner that it breaks the location of the control makes it so that some sites don't work. Here are some that I've tested that don't currently work:
+
+* Hulu
+* Hotstar
+* NBA.com
+* Polygon.com
+
+## Feedback
+
+Please feel free to file bugs but also understand that this is pretty much a side project. PRs are definately welcome.
